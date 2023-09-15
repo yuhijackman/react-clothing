e74b2fcb6d6feb3f6aa74a71878283ea02fb4394
yuhijackman <yuuhiandlisa@gmail.com>
2023-09-15 13:10:11 +0900

    Stricly type category action creator

--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -1,5 +1,10 @@
-import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
-import { CategoryAction } from "./category.action";
+import { AnyAction } from "redux";
+import { Category } from "./categories.types";
+import {
+  fetchCategoriesStart,
+  fetchCategoriesSuccess,
+  fetchCategoriesFailed
+} from "./category.action";
 
 export type CategoriesState = {
   readonly categories: Category[];
@@ -15,27 +20,28 @@ const INITIAL_STATE: CategoriesState = {
 
 export const categoriesReducer = (
   state = INITIAL_STATE,
-  action = {} as CategoryAction
+  action = {} as AnyAction
 ) => {
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return {
-        ...state,
-        isLoading: true
-      };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return {
-        ...state,
-        categories: action.payload,
-        isLoading: false
-      };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return {
-        ...state,
-        error: action.payload,
-        isLoading: false
-      };
-    default:
-      return state;
+  if (fetchCategoriesStart.match(action)) {
+    return {
+      ...state,
+      isLoading: true
+    };
   }
+  if (fetchCategoriesSuccess.match(action)) {
+    return {
+      ...state,
+      categories: action.payload,
+      isLoading: false
+    };
+  }
+  if (fetchCategoriesFailed.match(action)) {
+    return {
+      ...state,
+      error: action.payload,
+      isLoading: false
+    };
+  }
+
+  return state;
 };
--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,7 +1,8 @@
 import {
   createAction,
   Action,
-  ActionWithPayload
+  ActionWithPayload,
+  withMatcher
 } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
 
@@ -18,24 +19,21 @@ export type FetchCategoriesFailed = ActionWithPayload<
   Error
 >;
 
-export type CategoryAction =
-  | FetchCategoriesStart
-  | FetchCategoriesSuccess
-  | FetchCategoriesFailed;
-
-export const fetchCategoriesStart = (): FetchCategoriesStart => {
+export const fetchCategoriesStart = withMatcher((): FetchCategoriesStart => {
   return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
-};
+});
 
-export const fetchCategoriesSuccess = (
-  categoriesArray: Category[]
-): FetchCategoriesSuccess => {
-  return createAction(
-    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
-    categoriesArray
-  );
-};
+export const fetchCategoriesSuccess = withMatcher(
+  (categoriesArray: Category[]): FetchCategoriesSuccess => {
+    return createAction(
+      CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+      categoriesArray
+    );
+  }
+);
 
-export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed => {
-  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-};
+export const fetchCategoriesFailed = withMatcher(
+  (error: Error): FetchCategoriesFailed => {
+    return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+  }
+);
