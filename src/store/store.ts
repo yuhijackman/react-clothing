7cb87db1bdff2656c2ac5c42670d520c11c1c37f
yuhijackman <yuuhiandlisa@gmail.com>
2023-09-16 10:55:21 +0900

    Type store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,25 @@
-import { compose, createStore, applyMiddleware } from "redux";
+import { compose, createStore, applyMiddleware, Middleware } from "redux";
 import logger from "redux-logger";
 // logger: action dispatch前のstoreの状態とdispatch実行後の状態を見るのに便利
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 import createSagaMiddleware from "redux-saga";
 
 import { rootSaga } from "./root-saga";
 
-const persistConfig = {
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whiteList: (keyof RootState)[];
+};
+const persistConfig: ExtendedPersistConfig = {
   key: "root",
   storage,
   whiteList: ["cart"]
@@ -22,7 +33,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middlewares = [
   process.env.NODE_ENV === "development" && logger,
   sagaMiddleware
-].filter(Boolean);
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
